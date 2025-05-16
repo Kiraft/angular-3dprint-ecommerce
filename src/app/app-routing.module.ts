5a6f8430095fb54bdda27d448aa6af777d6958c3
Your Name <you@example.com>
2025-05-16 02:05:31 -0600

    estable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // layouts
-import { AdminComponent } from './layouts/admin/admin.component';
+
 import { AuthComponent } from './layouts/auth/auth.component';
 
-// admin views
-import { DashboardComponent } from './views/admin/dashboard/dashboard.component';
-import { MapsComponent } from './views/admin/maps/maps.component';
-import { SettingsComponent } from './views/admin/settings/settings.component';
-import { TablesComponent } from './views/admin/tables/tables.component';
+
 
 // auth views
 import { LoginComponent } from './views/auth/login/login.component';
@@ -25,18 +21,8 @@ import { ShippingComponent } from './views/account/shipping/shipping.component';
 import { CartSummaryComponent } from './layouts/cart-summary/cart-summary.component';
 
 const routes: Routes = [
-  // admin views
-  {
-    path: 'admin',
-    component: AdminComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'settings', component: SettingsComponent },
-      { path: 'tables', component: TablesComponent },
-      { path: 'maps', component: MapsComponent },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    ],
-  },
+
+
   // auth views
   {
     path: 'auth',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,43 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // layouts
-import { AdminComponent } from "./layouts/admin/admin.component";
-import { AuthComponent } from "./layouts/auth/auth.component";
+import { AuthComponent } from './layouts/auth/auth.component';
 
 // admin views
-import { DashboardComponent } from "./views/admin/dashboard/dashboard.component";
-import { MapsComponent } from "./views/admin/maps/maps.component";
-import { SettingsComponent } from "./views/admin/settings/settings.component";
-import { TablesComponent } from "./views/admin/tables/tables.component";
 
 // auth views
-import { LoginComponent } from "./views/auth/login/login.component";
-import { RegisterComponent } from "./views/auth/register/register.component";
+import { LoginComponent } from './views/auth/login/login.component';
+import { RegisterComponent } from './views/auth/register/register.component';
 
 // no layouts views
 
 // components for views and layouts
 
-import { AdminNavbarComponent } from "./components/navbars/admin-navbar/admin-navbar.component";
-import { CardBarChartComponent } from "./components/cards/card-bar-chart/card-bar-chart.component";
-import { CardLineChartComponent } from "./components/cards/card-line-chart/card-line-chart.component";
-import { CardPageVisitsComponent } from "./components/cards/card-page-visits/card-page-visits.component";
-import { CardProfileComponent } from "./components/cards/card-profile/card-profile.component";
-import { CardSettingsComponent } from "./components/cards/card-settings/card-settings.component";
-import { CardSocialTrafficComponent } from "./components/cards/card-social-traffic/card-social-traffic.component";
-import { CardStatsComponent } from "./components/cards/card-stats/card-stats.component";
-import { CardTableComponent } from "./components/cards/card-table/card-table.component";
-import { FooterAdminComponent } from "./components/footers/footer-admin/footer-admin.component";
-import { FooterComponent } from "./components/footers/footer/footer.component";
-import { FooterSmallComponent } from "./components/footers/footer-small/footer-small.component";
-import { HeaderStatsComponent } from "./components/headers/header-stats/header-stats.component";
-import { MapExampleComponent } from "./components/maps/map-example/map-example.component";
-import { IndexDropdownComponent } from "./components/dropdowns/index-dropdown/index-dropdown.component";
-import { TableDropdownComponent } from "./components/dropdowns/table-dropdown/table-dropdown.component";
-import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pages-dropdown.component";
-import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
-import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
+import { AdminNavbarComponent } from './components/navbars/admin-navbar/admin-navbar.component';
+import { CardBarChartComponent } from './components/cards/card-bar-chart/card-bar-chart.component';
+import { CardLineChartComponent } from './components/cards/card-line-chart/card-line-chart.component';
+import { CardPageVisitsComponent } from './components/cards/card-page-visits/card-page-visits.component';
+import { CardProfileComponent } from './components/cards/card-profile/card-profile.component';
+import { CardSettingsComponent } from './components/cards/card-settings/card-settings.component';
+import { CardSocialTrafficComponent } from './components/cards/card-social-traffic/card-social-traffic.component';
+import { CardStatsComponent } from './components/cards/card-stats/card-stats.component';
+import { CardTableComponent } from './components/cards/card-table/card-table.component';
+import { FooterAdminComponent } from './components/footers/footer-admin/footer-admin.component';
+import { FooterComponent } from './components/footers/footer/footer.component';
+import { FooterSmallComponent } from './components/footers/footer-small/footer-small.component';
+import { HeaderStatsComponent } from './components/headers/header-stats/header-stats.component';
+import { MapExampleComponent } from './components/maps/map-example/map-example.component';
+import { IndexDropdownComponent } from './components/dropdowns/index-dropdown/index-dropdown.component';
+import { TableDropdownComponent } from './components/dropdowns/table-dropdown/table-dropdown.component';
+import { PagesDropdownComponent } from './components/dropdowns/pages-dropdown/pages-dropdown.component';
+import { NotificationDropdownComponent } from './components/dropdowns/notification-dropdown/notification-dropdown.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { UserDropdownComponent } from './components/dropdowns/user-dropdown/user-dropdown.component';
 import { StockComponent } from './views/home/stock/stock.component';
 import { UploadComponent } from './views/home/upload/upload.component';
 import { HomeComponent } from './layouts/home/home.component';
@@ -58,11 +53,12 @@ import { CartSummaryComponent } from './layouts/cart-summary/cart-summary.compon
 import { GlobalNavbarComponent } from './components/navbars/global-navbar/global-navbar.component';
 import { CardCartComponent } from './components/cards/card-cart/card-cart.component';
 
+
 @NgModule({
   declarations: [
     CartSummaryComponent,
+    CardCartComponent,
     AppComponent,
-    DashboardComponent,
     CardBarChartComponent,
     CardLineChartComponent,
     IndexDropdownComponent,
@@ -83,11 +79,7 @@ import { CardCartComponent } from './components/cards/card-cart/card-cart.compon
     HeaderStatsComponent,
     MapExampleComponent,
     AdminNavbarComponent,
-    AdminComponent,
     AuthComponent,
-    MapsComponent,
-    SettingsComponent,
-    TablesComponent,
     LoginComponent,
     RegisterComponent,
     StockComponent,
@@ -102,7 +94,8 @@ import { CardCartComponent } from './components/cards/card-cart/card-cart.compon
     ProfileComponent,
     ShippingComponent,
     GlobalNavbarComponent,
-    CardCartComponent  ],
+    CardCartComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
